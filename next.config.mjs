6176f827032a8b25047ad7d5e55d39741ff9a228
Catalina Heredia <herediacatalina1995@gmail.com>
2024-09-03 17:01:31 -0300

    feature(cloudflare) add configuration to deploy in cloudflare

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,5 @@
+import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
@@ -5,5 +7,9 @@ const nextConfig = {
       domains: ['rickandmortyapi.com'],
     },
 };
+
+if (process.env.NODE_ENV === 'development') {
+  await setupDevPlatform();
+}
 
 export default nextConfig;
